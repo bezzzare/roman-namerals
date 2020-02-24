5119502a01614ac6eb2422ed8541471a096452ee
bezzzare <bezzzare@inbox.ru>
2020-02-24 15:45:04 +0300

    Add arguments check and comments

--- scripts/RomanNumerals.js
+++ scripts/RomanNumerals.js
@@ -36,7 +36,16 @@ const TO_ROMAN_MAP = Object.entries(FROM_ROMAN_MAP).reduce((accum, [key, value])
   return accum
 }, {})
 
-const toRoman = (num) => {
+/**
+ *
+ * Converts normal number to string that represents Roman number
+ * @param {number} num Normal number from 0 to 3999
+ * @return {string} Returns string that represents Roman number
+ */
+const toRoman = num => {
+  if (!Number.isInteger(num) || num < 0 || num > 3999)
+    throw new TypeError('fromRoman function accepts number from 0 to 3999')
+
   const strNum = num.toString()
   let output = ''
   for (let i = 0; i < strNum.length; i++) {
@@ -48,9 +57,17 @@ const toRoman = (num) => {
   }
   return output
 }
-
-const fromRoman = (romanNumber) => {
+/**
+ *
+ * Converts string that represents Roman number to common number
+ * @param {string} romanNumber String that represents Roman number
+ * @return {number} Returns number or -1 if could not convert
+ */
+const fromRoman = romanNumber => {
+  if (typeof romanNumber !== 'string' && !(romanNumber instanceof String))
+    throw new TypeError('fromRoman function accepts string only')
   if (!romanNumber) return 0
+
   const romanChars = romanNumber.split('')
   const groups = [1000, 100, 10, 1]
 
